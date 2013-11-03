b224c4082222beae2862cb8ad0d8f781ca7d4e6d
freemanoid <freemanoid321@gmail.com>
2013-11-03 17:23:38 +0300

    Move UrlParser to Truncator module as it should be

--- lib/truncator/url_parser.rb
+++ lib/truncator/url_parser.rb
@@ -1,154 +1,156 @@
-class UrlParser
-  class << self
-    URL_VALID_SYMBOLS = %Q{[#{Regexp.escape('!#$&-;=?-[]_~')}a-zA-Z0-9]}
-    SEPARATOR = '...'
+module Truncator
+  class UrlParser
+    class << self
+      URL_VALID_SYMBOLS = %Q{[#{Regexp.escape('!#$&-;=?-[]_~')}a-zA-Z0-9]}
+      SEPARATOR = '...'
 
-    def shorten_url(url, truncation_length = 42)
-      url = url.dup
-      if not ordinary_hostname?(url)
-        url = truncate_all_params_except_first(url) if url[-1] != '/'
-        return url
-      end
+      def shorten_url(url, truncation_length = 42)
+        url = url.dup
+        if not ordinary_hostname?(url)
+          url = truncate_all_params_except_first(url) if url[-1] != '/'
+          return url
+        end
 
-      url.sub!(/\Ahttp:\/\//, '') # remove http protocol
-      url.gsub(/\/{2,}/, '/') # replace multiple slashes by one
-      url.chomp!('/') # remove slash from the end
-      return url if valid_length?(url.length, truncation_length)
+        url.sub!(/\Ahttp:\/\//, '') # remove http protocol
+        url.gsub(/\/{2,}/, '/') # replace multiple slashes by one
+        url.chomp!('/') # remove slash from the end
+        return url if valid_length?(url.length, truncation_length)
 
-      if !has_path?(url)
-        url = truncate(url, truncation_length)
-        return url
-      end
-
-      if params_exists?(url)
-        if invalid_length?(hostname(url).length, truncation_length) and last_directory(url).length > truncation_length
-          url = truncate_last_directory(url, truncation_length)
-        elsif valid_length?(url.length - last_directory(url).length, truncation_length) or !has_dirs?(url)
+        if !has_path?(url)
           url = truncate(url, truncation_length)
+          return url
         end
-      else
-        if valid_length?(hostname(url).length, truncation_length)
-          url = truncate_by_shortest(url, truncation_length)
+
+        if params_exists?(url)
+          if invalid_length?(hostname(url).length, truncation_length) and last_directory(url).length > truncation_length
+            url = truncate_last_directory(url, truncation_length)
+          elsif valid_length?(url.length - last_directory(url).length, truncation_length) or !has_dirs?(url)
+            url = truncate(url, truncation_length)
+          end
         else
-          url = truncate_all_directories(url)
-          url = truncate_last_directory(url, truncation_length)
-        end
-      end
-
-      url
-    end
-
-    private
-      ## FIXME: add to String#truncate
-      def truncate(str, length)
-        str = str.dup
-        if str.length > length
-          str[(length - SEPARATOR.length)..-1] = SEPARATOR
-        end
-        str
-      end
-
-      def truncate_all_params_except_first(url)
-        url.dup
-        url[url.index('&')..-1] = SEPARATOR
-        url
-      end
-
-      def ordinary_hostname?(url)
-        if url.start_with?('https://')
-          # https
-          false
-        elsif url =~ /^http[s]?:\/\/[a-zA-Z\d.]+:\d{1,5}\//
-          # has port
-          false
-        elsif url.start_with?('ftp://')
-          # ftp
-          false
-        elsif url =~ /^http:\/\/[a-zA-Z\d]+:[a-zA-Z\d]+@[a-zA-Z\d.]+\//
-          # with credentials
-          false
-        else
-          true
-        end
-      end
-
-      def hostname(url)
-        url[0..(url.index('/') - 1)]
-      end
-
-      def valid_length?(url_length, truncation_length)
-        url_length <= truncation_length
-      end
-
-      def invalid_length?(url_length, truncation_length)
-        !valid_length?(url_length, truncation_length)
-      end
-
-      # Check if url has query parameters
-      def params_exists?(url)
-        valid_symbols_before_params = URL_VALID_SYMBOLS.dup.tap { |r| r[-1] = '.\/]' }
-        !!(url =~ /^#{valid_symbols_before_params}+\?(#{URL_VALID_SYMBOLS}+)/)
-      end
-
-      def params(url)
-        valid_symbols_before_params = URL_VALID_SYMBOLS.dup.tap { |r| r[-1] = '.\/]' }
-        url.match(/^#{valid_symbols_before_params}+\?(#{URL_VALID_SYMBOLS}+)/)[1]
-      end
-
-      # Get the 'directories' of the link
-      def directories(url)
-        url.to_enum(:scan, /(?<=\/)(#{URL_VALID_SYMBOLS}+)(?=\/)/).map { Regexp.last_match }
-      end
-
-      def truncate_all_directories(url)
-        first_slash = url.index('/')
-        last_slash = url.rindex('/')
-        url = url.dup
-        url[first_slash..last_slash] = "/#{SEPARATOR}/"
-        url
-      end
-
-      def last_directory(url)
-        url[(url.rindex('/') + 1)..-1]
-      end
-
-      def truncate_last_directory(url, truncation_length)
-        last_dir_begin = url.rindex('/') + 1
-        last_dir = last_directory(url)
-        url = url.dup
-        url[last_dir_begin..-1] = truncate(last_dir, truncation_length)
-        url
-      end
-
-      # If url has any 'dirs' this is suburls between '/'
-      def has_dirs?(url)
-        url.count('/') > 1
-      end
-
-      def has_path?(url)
-        url.count('/') > 0
-      end
-
-      def truncate_by_shortest(url, target_length)
-        url = url.dup
-        dirs = directories(url)
-        dirs_sorted = dirs.sort_by { |d| d.to_s.length }
-
-        # Get the shortest dir and try to truncate the url by this dir and nexts
-        dirs_sorted.each do |next_min_dir|
-          current_dirs_length = 0
-          current_dirs_count = 0
-          dirs[dirs.index(next_min_dir)..-1].each do |dir|
-            current_dirs_length += dir.to_s.length
-            current_dirs_count += 1
-            current_dirs_length_with_slashes = current_dirs_length + (current_dirs_count - 1) - SEPARATOR.length
-            if valid_length?(url.length - current_dirs_length_with_slashes, target_length)
-              url[next_min_dir.begin(0)..(dirs[dirs.index(next_min_dir) + (current_dirs_count - 1)].end(0) - 1)] = SEPARATOR
-              return url
-            end
+          if valid_length?(hostname(url).length, truncation_length)
+            url = truncate_by_shortest(url, truncation_length)
+          else
+            url = truncate_all_directories(url)
+            url = truncate_last_directory(url, truncation_length)
           end
         end
+
         url
       end
+
+      private
+        ## FIXME: add to String#truncate
+        def truncate(str, length)
+          str = str.dup
+          if str.length > length
+            str[(length - SEPARATOR.length)..-1] = SEPARATOR
+          end
+          str
+        end
+
+        def truncate_all_params_except_first(url)
+          url.dup
+          url[url.index('&')..-1] = SEPARATOR
+          url
+        end
+
+        def ordinary_hostname?(url)
+          if url.start_with?('https://')
+            # https
+            false
+          elsif url =~ /^http[s]?:\/\/[a-zA-Z\d.]+:\d{1,5}\//
+            # has port
+            false
+          elsif url.start_with?('ftp://')
+            # ftp
+            false
+          elsif url =~ /^http:\/\/[a-zA-Z\d]+:[a-zA-Z\d]+@[a-zA-Z\d.]+\//
+            # with credentials
+            false
+          else
+            true
+          end
+        end
+
+        def hostname(url)
+          url[0..(url.index('/') - 1)]
+        end
+
+        def valid_length?(url_length, truncation_length)
+          url_length <= truncation_length
+        end
+
+        def invalid_length?(url_length, truncation_length)
+          !valid_length?(url_length, truncation_length)
+        end
+
+        # Check if url has query parameters
+        def params_exists?(url)
+          valid_symbols_before_params = URL_VALID_SYMBOLS.dup.tap { |r| r[-1] = '.\/]' }
+          !!(url =~ /^#{valid_symbols_before_params}+\?(#{URL_VALID_SYMBOLS}+)/)
+        end
+
+        def params(url)
+          valid_symbols_before_params = URL_VALID_SYMBOLS.dup.tap { |r| r[-1] = '.\/]' }
+          url.match(/^#{valid_symbols_before_params}+\?(#{URL_VALID_SYMBOLS}+)/)[1]
+        end
+
+        # Get the 'directories' of the link
+        def directories(url)
+          url.to_enum(:scan, /(?<=\/)(#{URL_VALID_SYMBOLS}+)(?=\/)/).map { Regexp.last_match }
+        end
+
+        def truncate_all_directories(url)
+          first_slash = url.index('/')
+          last_slash = url.rindex('/')
+          url = url.dup
+          url[first_slash..last_slash] = "/#{SEPARATOR}/"
+          url
+        end
+
+        def last_directory(url)
+          url[(url.rindex('/') + 1)..-1]
+        end
+
+        def truncate_last_directory(url, truncation_length)
+          last_dir_begin = url.rindex('/') + 1
+          last_dir = last_directory(url)
+          url = url.dup
+          url[last_dir_begin..-1] = truncate(last_dir, truncation_length)
+          url
+        end
+
+        # If url has any 'dirs' this is suburls between '/'
+        def has_dirs?(url)
+          url.count('/') > 1
+        end
+
+        def has_path?(url)
+          url.count('/') > 0
+        end
+
+        def truncate_by_shortest(url, target_length)
+          url = url.dup
+          dirs = directories(url)
+          dirs_sorted = dirs.sort_by { |d| d.to_s.length }
+
+          # Get the shortest dir and try to truncate the url by this dir and next
+          dirs_sorted.each do |next_min_dir|
+            current_dirs_length = 0
+            current_dirs_count = 0
+            dirs[dirs.index(next_min_dir)..-1].each do |dir|
+              current_dirs_length += dir.to_s.length
+              current_dirs_count += 1
+              current_dirs_length_with_slashes = current_dirs_length + (current_dirs_count - 1) - SEPARATOR.length
+              if valid_length?(url.length - current_dirs_length_with_slashes, target_length)
+                url[next_min_dir.begin(0)..(dirs[dirs.index(next_min_dir) + (current_dirs_count - 1)].end(0) - 1)] = SEPARATOR
+                return url
+              end
+            end
+          end
+          url
+        end
+    end
   end
 end
--- spec/url_parser_spec.rb
+++ spec/url_parser_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 require 'truncator'
 
-describe UrlParser do
+describe Truncator::UrlParser do
 
   describe ".shorten_url" do
 
     context "when no truncation length is specified, the URL is too long, has sublevels, but no query params" do
-      let(:shortened) { UrlParser.shorten_url "http://www.foo.com/this/is/a/b/c/d/e/f/string.html" }
+      let(:shortened) { Truncator::UrlParser.shorten_url "http://www.foo.com/this/is/a/b/c/d/e/f/string.html" }
 
       it "should default to truncate at 42 characters" do
         # shortened.should == "www.foo.com/this/is/a/.../e/f/string.html"
@@ -15,7 +15,7 @@ describe UrlParser do
     end
 
     context 'when URL has a large last folder level' do
-      let(:shortened) { UrlParser.shorten_url "http://m.whitehouse.gov/blog/2013/08/23/check-out-secretary-education-arne-duncans-google-hangout-sal-khan?utm_source=email&utm_medium=email&utm_content=email232-graphic2&utm_campaign=education" }
+      let(:shortened) { Truncator::UrlParser.shorten_url "http://m.whitehouse.gov/blog/2013/08/23/check-out-secretary-education-arne-duncans-google-hangout-sal-khan?utm_source=email&utm_medium=email&utm_content=email232-graphic2&utm_campaign=education" }
 
       it "should should default to truncate at 42 characters" do
         shortened.should == "m.whitehouse.gov/blog/2013/08/23/check-..."
@@ -24,7 +24,7 @@ describe UrlParser do
 
     context "when URL is too long, has sublevels, but no query params" do
       it "should ellipse the directories and just show the file" do
-        UrlParser.shorten_url("http://www.foo.com/this/is/a/really/long/url/that/has/no/query/string.html", 30).should == "www.foo.com/.../string.html"
+        Truncator::UrlParser.shorten_url("http://www.foo.com/this/is/a/really/long/url/that/has/no/query/string.html", 30).should == "www.foo.com/.../string.html"
       end
 
       it "should replace path segments with ellipses to shorten the path as much as necessary" do
@@ -34,7 +34,7 @@ describe UrlParser do
         0.upto(20) do |n|
           url = url_prefix + url_middle + url_suffix
           url_middle += "and/on/"
-          shorter_url = UrlParser.shorten_url(url, 30)
+          shorter_url = Truncator::UrlParser.shorten_url(url, 30)
           #TODO: rewrite this
           shorter_url.should == if url[7..-1].length <= 30
                                   url[7..-1]
@@ -51,7 +51,7 @@ describe UrlParser do
         0.upto(20) do |n|
           url = url_prefix + url_middle + url_suffix
           url_middle += "nd/on/"
-          shorter_url = UrlParser.shorten_url(url, 30)
+          shorter_url = Truncator::UrlParser.shorten_url(url, 30)
           #TODO: rewrite this
           shorter_url.should == if url[7..-1].length <= 30
                                   url[7..-1]
@@ -63,16 +63,16 @@ describe UrlParser do
 
       # it "should replace path segments with ellipses to shorten the path as much as necessary for various lengths", focus: true do
       #   url = "http://www.foo.com/this/goes/on/and/on/and/on/with/XXX.html"
-      #   UrlParser.shorten_url(url, 24).should == "www.foo.com/.../XXX.html"
-      #   32.upto(33) { |n| UrlParser.shorten_url(url, n).should == "www.foo.com/.../on/with/XXX.html" }
-      #   35.upto(39) { |n| UrlParser.shorten_url(url, n).should == "www.foo.com/this/.../with/XXX.html" }
-      #   40.upto(42) { |n| UrlParser.shorten_url(url, n).should == "www.foo.com/this/goes/.../with/XXX.html" }
-      #   43.upto(45) { |n| UrlParser.shorten_url(url, n).should == "www.foo.com/this/goes/.../on/with/XXX.html" }
+      #   Truncator::UrlParser.shorten_url(url, 24).should == "www.foo.com/.../XXX.html"
+      #   32.upto(33) { |n| Truncator::UrlParser.shorten_url(url, n).should == "www.foo.com/.../on/with/XXX.html" }
+      #   35.upto(39) { |n| Truncator::UrlParser.shorten_url(url, n).should == "www.foo.com/this/.../with/XXX.html" }
+      #   40.upto(42) { |n| Truncator::UrlParser.shorten_url(url, n).should == "www.foo.com/this/goes/.../with/XXX.html" }
+      #   43.upto(45) { |n| Truncator::UrlParser.shorten_url(url, n).should == "www.foo.com/this/goes/.../on/with/XXX.html" }
       # end
     end
 
     # context "when URL is too long and has at least one sublevel specified as well as a query parameter" do
-    #   let(:shortened) { UrlParser.shorten_url("http://www.foo.com/this/goes/on/and/on/and/on/and/on/and/ends/with/XXXX.html?q=1&a=2&b=3", 50) }
+    #   let(:shortened) { Truncator::UrlParser.shorten_url("http://www.foo.com/this/goes/on/and/on/and/on/and/on/and/ends/with/XXXX.html?q=1&a=2&b=3", 50) }
 
     #   it "should replace path segments with ellipses to shorten the path as much as necessary, and show only the first param, followed by ellipses" do
     #     shortened.should == "www.foo.com/this/goes/.../with/XXXX.html?q=1..."
@@ -80,7 +80,7 @@ describe UrlParser do
     # end
 
     context "when URL is more than 30 chars long and does not have at least one sublevel specified" do
-      let(:shortened) { UrlParser.shorten_url("http://www.mass.gov/?pageID=trepressrelease&L=4&L0=Home&L1=Media+%26+Publications&L2=Treasury+Press+Releases&L3=2006&sid=Ctre&b=pressrelease&f=2006_032706&csid=Ctre", 30) }
+      let(:shortened) { Truncator::UrlParser.shorten_url("http://www.mass.gov/?pageID=trepressrelease&L=4&L0=Home&L1=Media+%26+Publications&L2=Treasury+Press+Releases&L3=2006&sid=Ctre&b=pressrelease&f=2006_032706&csid=Ctre", 30) }
 
       it "should truncate to 30 chars with ellipses" do
         shortened.should == "www.mass.gov/?pageID=trepre..."
@@ -88,7 +88,7 @@ describe UrlParser do
     end
 
     context "when the URL contains a really long host name and a long trailing filename" do
-      let(:shortened) { UrlParser.shorten_url("http://www128376218.skjdhfskdjfhs.lqdkwjqlkwjdqlqwkjd.com/some/path/1234567890123456789012345678901234test_of_the_mergency_broadcastingnet_work.html", 30) }
+      let(:shortened) { Truncator::UrlParser.shorten_url("http://www128376218.skjdhfskdjfhs.lqdkwjqlkwjdqlqwkjd.com/some/path/1234567890123456789012345678901234test_of_the_mergency_broadcastingnet_work.html", 30) }
 
       it "should not truncate the host name and truncated the last part of the path to truncation length and all dirs" do
         shortened.should == "www128376218.skjdhfskdjfhs.lqdkwjqlkwjdqlqwkjd.com/.../123456789012345678901234567..."
@@ -97,7 +97,7 @@ describe UrlParser do
 
     #TODO: this and the next spec can be merged
     context "when the URL contains a really long host name and is an http url and has an empty path" do
-      let(:shortened) { UrlParser.shorten_url("http://www128376218.skjdhfskdj.lqdkwjqlkwjdqlqwkjd.com/", 30) }
+      let(:shortened) { Truncator::UrlParser.shorten_url("http://www128376218.skjdhfskdj.lqdkwjqlkwjdqlqwkjd.com/", 30) }
 
       it "should truncate the host name and have no trailing /" do
         shortened.should == "www128376218.skjdhfskdj.lqd..."
@@ -105,7 +105,7 @@ describe UrlParser do
     end
 
     context "when the URL contains a really long host name and is an http url and has an empty path and no trailing /" do
-      let(:shortened) { UrlParser.shorten_url("http://www128376218.skjdhfskdj.lqdkwjqlkwjdqlqwkjd.com", 30) }
+      let(:shortened) { Truncator::UrlParser.shorten_url("http://www128376218.skjdhfskdj.lqdkwjqlkwjdqlqwkjd.com", 30) }
 
       it "should truncate the URL and have no trailing /" do
         shortened.should == "www128376218.skjdhfskdj.lqd..."
@@ -113,7 +113,7 @@ describe UrlParser do
     end
 
     context "when the URL contains a really long host name and has a really long query parameter" do
-      let(:shortened) { UrlParser.shorten_url("http://www128376218.skjdhfskdjfhs.lqdkwjqlkwjdqlqwkjd.com/?cmd=1234567890123456789012345678901234&api_key=1234567890123456789012345678901234", 30) }
+      let(:shortened) { Truncator::UrlParser.shorten_url("http://www128376218.skjdhfskdjfhs.lqdkwjqlkwjdqlqwkjd.com/?cmd=1234567890123456789012345678901234&api_key=1234567890123456789012345678901234", 30) }
 
       it "should not truncate the host name but truncate the query parameter" do
         shortened.should == "www128376218.skjdhfskdjfhs.lqdkwjqlkwjdqlqwkjd.com/?cmd=1234567890123456789012..."
@@ -121,7 +121,7 @@ describe UrlParser do
     end
 
     context "when URL is really short and contains only the protocol http and hostname" do
-      let(:shortened) { UrlParser.shorten_url("http://bit.ly/") }
+      let(:shortened) { Truncator::UrlParser.shorten_url("http://bit.ly/") }
 
       it "should omit the protocol as well as trailing slash" do
         shortened.should == "bit.ly"
@@ -129,7 +129,7 @@ describe UrlParser do
     end
 
     context "when URL is really short and contains only the protocol http and hostname and a long query parameter" do
-      let(:shortened) { UrlParser.shorten_url("http://api.bit.ly/?cmd=boom&t=now&auth_token=f886c1c02896492577e92b550cd22b3c83b062") }
+      let(:shortened) { Truncator::UrlParser.shorten_url("http://api.bit.ly/?cmd=boom&t=now&auth_token=f886c1c02896492577e92b550cd22b3c83b062") }
 
       it "should omit the protocol as well as trailing slash" do
         shortened.should == "api.bit.ly/?cmd=boom&t=now&auth_token=f..."
@@ -137,7 +137,7 @@ describe UrlParser do
     end
 
     context "when URL is really short and contains only the protocol http and hostname and a short query parameter" do
-      let(:shortened) { UrlParser.shorten_url("http://api.bit.ly/?cmd=boom&t=now") }
+      let(:shortened) { Truncator::UrlParser.shorten_url("http://api.bit.ly/?cmd=boom&t=now") }
 
       it "should omit the protocol as well as trailing slash" do
         shortened.should == "api.bit.ly/?cmd=boom&t=now"
@@ -168,7 +168,7 @@ describe UrlParser do
 
       it "should truncate to 30 chars with ellipses" do
         @long_urls.each_with_index do |url, x|
-          UrlParser.shorten_url(url, 30).should == @short_urls[x]
+          Truncator::UrlParser.shorten_url(url, 30).should == @short_urls[x]
         end
       end
     end
